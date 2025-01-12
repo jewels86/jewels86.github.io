abdbc9fa130692116469d8f03889f65673291cf2
Jewels <jewels286@proton.me>
2025-01-12 09:09:50 -0500

    solution-specific urls sort of work

--- resources/js/solutions.js
+++ resources/js/solutions.js
@@ -18,7 +18,7 @@ async function main() {
     });
 
     const urlParams = new URLSearchParams(window.location.search);
-    const solutionName = urlParams.get('s').toLowerCase();
+    const solutionName = urlParams.get('s');
 
     const statusToColor = {
         'a-m': 'green',
@@ -38,10 +38,27 @@ async function main() {
         const element = document.createElement('div');
         element.classList.add('solution-detail');
         element.innerHTML = `
-        <h2 class="${solution.color}">${solution.name}</h2>
-        <p>${solution.description}</p>
-        <p>License: ${solution.license}<p>
-        <p>Status: <span class="${statusToColor[solution.status]}">${statusToText[solution.status]}</span></p>
+        <div>
+            <h2 class="${solution.color}">${solution.name}</h2>
+            <p>${solution.description}</p>
+            <p>License: ${solution.license}<p>
+            <p>Status: <span class="${statusToColor[solution.status]}">${statusToText[solution.status]}</span></p>
+        </div>
+        <div>
+            <ul>
+                <li>Languages: ${solution.languages.join(', ')}</li>
+                <li>Keywords: ${solution.keywords.join(', ')}</li>
+                <li>Platforms: ${solution.platforms.join(', ')}</li>
+                <li>License: ${solution.license}</li>
+                <li>Version: ${solution.version}</li>
+                <li>Authors: ${solution.authors.join(', ')}</li>
+                <li>Links:</li>
+                <ul>
+                    ${solution.links.map(link => `<li><a href="${link[1]}">${link[0]}</a></li>`).join('')}
+                </ul>
+                <li>Installation: ${solution.installation}</li>
+            </ul>
+        </div>
         `
         document.getElementById('body').append(element);
     }
